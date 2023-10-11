5bc3f6b7b6d661c1ff13f7508399545e0696a783
Abhimanyu Aryan <8083613+AbhimanyuAryan@users.noreply.github.com>
2023-10-11 15:54:05 +0000

    automatic backup

---
+++ new/Cute theory.jl
@@ -0,0 +1,11 @@
+### A Pluto.jl notebook ###
+# v0.19.42
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 58120012-684e-11ee-2ded-c729413a37f1
+
+
+# ╔═╡ Cell order:
+# ╠═58120012-684e-11ee-2ded-c729413a37f1
