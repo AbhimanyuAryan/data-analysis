ab7150f297342637316cdc6bcae0e5f2e43fe324
Abhimanyu Aryan <8083613+AbhimanyuAryan@users.noreply.github.com>
2023-10-11 15:55:20 +0000

    automatic backup

--- new/Cute theory.jl
+++ new/Cute theory.jl
@@ -7,13 +7,9 @@ using InteractiveUtils
 # ╔═╡ 58120012-684e-11ee-2ded-c729413a37f1
 x = 23;
 
-# ╔═╡ a43e83f0-0c39-48ba-ae7d-a4a13e38f646
-println(x)
-
 # ╔═╡ 3ebf7ea2-2999-4051-a882-64080797cedb
 
 
 # ╔═╡ Cell order:
 # ╠═58120012-684e-11ee-2ded-c729413a37f1
-# ╠═a43e83f0-0c39-48ba-ae7d-a4a13e38f646
 # ╠═3ebf7ea2-2999-4051-a882-64080797cedb
