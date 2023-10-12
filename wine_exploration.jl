0a4e2433b819ba735bdb097811eb0650713adaab
Abhimanyu Aryan <8083613+AbhimanyuAryan@users.noreply.github.com>
2023-10-12 15:01:23 +0000

    automatic backup

--- wine_exploration.jl
+++ wine_exploration.jl
@@ -7,5 +7,9 @@ using InteractiveUtils
 # ╔═╡ 0217157c-6910-11ee-3c7c-cb59227a95b6
 x = 5;
 
+# ╔═╡ cb4e92cb-2ed6-4b97-a844-d597d9b98954
+
+
 # ╔═╡ Cell order:
 # ╠═0217157c-6910-11ee-3c7c-cb59227a95b6
+# ╠═cb4e92cb-2ed6-4b97-a844-d597d9b98954
