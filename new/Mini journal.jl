621b23830daf77769a12a4b3558694f77b9dd42e
Abhimanyu Aryan <8083613+AbhimanyuAryan@users.noreply.github.com>
2023-10-12 15:00:17 +0000

    automatic backup

---
+++ new/Mini journal.jl
@@ -0,0 +1,11 @@
+### A Pluto.jl notebook ###
+# v0.19.42
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 0217157c-6910-11ee-3c7c-cb59227a95b6
+
+
+# ╔═╡ Cell order:
+# ╠═0217157c-6910-11ee-3c7c-cb59227a95b6
